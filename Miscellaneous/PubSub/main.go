aaf6980ba6e607be47c6ed41d0686e4b45e620cf
youshy <devilyoushy@gmail.com>
2019-12-18 15:01:19 +0000

    Simple PubSub system

---
+++ Miscellaneous/PubSub/main.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+	"time"
+)
+
+type DataEvent struct {
+	Data  interface{}
+	Topic string
+}
+
+// DataChannel is a channel which can accept an DataEvent
+type DataChannel chan DataEvent
+
+// DataChannelSlice is a slice of DataChannels
+type DataChannelSlice []DataChannel
+
+// EventBus stores the information about subscribers interested for a particular topic
+type EventBus struct {
+	subscribers map[string]DataChannelSlice
+	rm          sync.RWMutex
+}
+
+func (eb *EventBus) Publish(topic string, data interface{}) {
+	eb.rm.RLock()
+	if chans, found := eb.subscribers[topic]; found {
+		// Slices refer to the same array that's why we create a new slice to make sure we're locking mutex correctly
+		channels := append(DataChannelSlice{}, chans...)
+		go func(data DataEvent, dataChannelSlices DataChannelSlice) {
+			for _, ch := range dataChannelSlices {
+				ch <- data
+			}
+		}(DataEvent{Data: data, Topic: topic}, channels)
+	}
+	eb.rm.RUnlock()
+}
+
+func (eb *EventBus) Subscribe(topic string, ch DataChannel) {
+	eb.rm.Lock()
+	if prev, found := eb.subscribers[topic]; found {
+		eb.subscribers[topic] = append(prev, ch)
+	} else {
+		eb.subscribers[topic] = append([]DataChannel{}, ch)
+	}
+	eb.rm.Unlock()
+}
+
+var eb = &EventBus{
+	subscribers: map[string]DataChannelSlice{},
+}
+
+func printDataEvent(ch string, data DataEvent) {
+	fmt.Printf("Channel: %s; Topic: %s; DataEvent: %v\n", ch, data.Topic, data.Data)
+}
+
+func publishTo(topic string, data string) {
+	for {
+		eb.Publish(topic, data)
+		time.Sleep(time.Duration(rand.Intn(1000)) * time.Millisecond)
+	}
+}
+
+func main() {
+	ch1 := make(chan DataEvent)
+	ch2 := make(chan DataEvent)
+	ch3 := make(chan DataEvent)
+
+	eb.Subscribe("topic1", ch1)
+	eb.Subscribe("topic2", ch2)
+	eb.Subscribe("topic3", ch3)
+
+	go publishTo("topic1", "Hello topic 1!")
+	go publishTo("topic2", "I A M T H E O N L Y V A L I D T O P I C")
+
+	for {
+		select {
+		case d := <-ch1:
+			go printDataEvent("ch1", d)
+		case d := <-ch2:
+			go printDataEvent("ch2", d)
+		case d := <-ch3:
+			go printDataEvent("ch3", d)
+		}
+	}
+}
